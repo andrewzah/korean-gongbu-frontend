a136d48e0964006fdc5721c888bb38e27d57b5ee
Andrew Zah <zah@andrewzah.com>
2020-07-26 16:46:50 -0400

    update .idea/ configurations

--- backend/src/handlers/status_handler.rs
+++ backend/src/handlers/status_handler.rs
@@ -5,10 +5,10 @@ use crate::errors::{AppError, AppResult};
 
 pub async fn sleepy(seconds: u64) -> AppResult {
     tokio::time::delay_for(Duration::from_secs(seconds)).await;
-    let response = String::from("{}");
+    let response = String::from("{\"data\": \"ok!\"}");
 
-    let _test_path = Path::new("/tmp/a").canonicalize()
-        .map_err(AppError::from)?;
+    // let _test_path = Path::new("/tmp/a").canonicalize()
+     //   .map_err(AppError::from)?;
 
     Ok(warp::reply::json(&response))
 }
